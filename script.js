5ea44075a265e396b1fa4a2578fbd8183581c748
Om-Mandlik <ommandlik702@gmail.com>
2026-02-11 18:07:00 +0530

    age logic

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ function calculate(){
     var result = document.getElementById("result")
     const birth_date = new Date(date)
     const today = new Date()
-    console.log(birth_date,today)
-    let age
-    age = today - birth_date 
-    console.log(age)
-
     
-   
+    
+    let ty = today.getFullYear()
+    let tm = today.getMonth()
+    let td = today.getDate()
+    let by = birth_date.getFullYear()
+    let bm = birth_date.getMonth()
+    let bd = birth_date.getDate()
+    let Dates,Months,years
+    Dates = (td - bd)
+    Months = (tm - bm)
+    years = (ty - by)
+    
 
+    if(years = 0){
+        result.innerHTML = "please enter valid date";
+    }
+
+    if (Dates < 0 ) {
+        let ndays =  new Date(ty,(tm-1),0).getDate()
+        Dates = (ndays+td) - bd
+        tm = tm - 1
+    }    
+    if (Months < 0){
+            Months = (tm + 12) - (bm)
+            years = (ty - 1) - (by)   
+        }
+    result.innerHTML = `${years} Years ${Months} Months ${Dates}days`
 }
+
 
